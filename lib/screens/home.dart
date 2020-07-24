0bba874213a0b3da4481ca89572d460380ee6746
V1PERFX <rafael_fiuza@ymail.com>
2020-07-23 23:07:55 -0300

    Começando o layout do app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,8 +3,34 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          height: 25,
+          child: Image.asset("images/ft_logo_rgb_dark.png"),
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black87,
+        actions: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text("0"),
+          ),
+          IconButton(
+            icon: Icon(Icons.star),
+            onPressed: (){
+
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: (){
+              
+            },
+          ),
+        ],
+      ),
+      body: Container(),
     );
   }
 }
